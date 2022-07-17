35ee9d63591022662af89c703d5a37ab53555d99
beginhope <109449506+beginhope@users.noreply.github.com>
2022-07-17 15:24:51 +0900

    led.3

--- led/led.ino
+++ led/led.ino
@@ -7,4 +7,15 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(7, HIGH);
+  delay(1000);
+  digitalWrite(7, LOW);
+  delay(1000);
+for(int i=0; i<255; i++){
+analogWrite(3, 255);
+delay(10);
+}
+for(int i=255; i>0; i--){
+  analogWrite(3, i);
+  delay(10);
+}
 }
